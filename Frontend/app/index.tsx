5154d68f5aa42920d26fb54543c8c225e42f87a7
Mostafa Mohamed <122822269+Mostafa602@users.noreply.github.com>
2025-12-05 22:19:08 +0200

    Delete Frontend directory

--- Frontend/app/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-import {Link} from "expo-router"
-export default function Index() {
-  return (
-    <View className="">
-      <Link href="/sign_in"><Text className="text-2xl font-bold">Hello</Text></Link>
-    </View>
-  );
-}
