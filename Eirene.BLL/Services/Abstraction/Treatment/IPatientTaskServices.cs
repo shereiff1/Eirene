496b902df2e67d8e80a6d479e575ef74d1e29f9b
shereiff1 <147086400+shereiff1@users.noreply.github.com>
2026-05-15 23:39:31 +0300

    adding questions to the answer to get the analysis result, and storing the tasks in DB

--- Eirene.BLL/Services/Abstraction/Treatment/IPatientTaskServices.cs
+++ Eirene.BLL/Services/Abstraction/Treatment/IPatientTaskServices.cs
@@ -1,13 +1,15 @@
-
-
 using Eirene.BLL.Models.Treatment.Task;
 using Eirene.BLL.ModelVMs.Treatment;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Eirene.BLL.Services.Abstraction.Treatment;
 
 public interface IPatientTaskServices
 {
-
     Task<bool> AddTasksFromModelAsync(string modelResult, string userId);
-
+    Task<IEnumerable<PatientTaskResponseDTO>> GetTasksForUserAsync(string userId);
+    Task<PatientTaskResponseDTO?> GetTaskByIdAsync(Guid taskId);
+    Task<bool> UpdateTaskStatusAsync(Guid taskId, bool isCompleted);
 }
